4c8c9448b534f117ab63452a397249747d36cfe3
gusainDeekshu <gusaindeekshant@gmail.com>
2025-02-07 07:21:41 +0530

    Added docker configuration

--- backend/client.js
+++ backend/client.js
@@ -1,8 +1,13 @@
 const Redis = require("ioredis");
 
+require("dotenv").config();
+// Create a Redis client using environment variables
+const client = new Redis({
+    host: process.env.REDIS_HOST || "localhost", // Redis server host
+    port: process.env.REDIS_PORT || 6379,       // Redis server port
+  });
 
-
-const client = new Redis();
+// const client = new Redis();
 
 
 module.exports=client;
